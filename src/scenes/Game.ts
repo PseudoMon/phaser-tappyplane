fdfe56874f58b1a7187bece866ef184d82e219bc
PseudoMon <aliya.nisa@yahoo.com>
2022-09-14 14:52:58 +0700

    Plane moved to its own object class

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,4 +1,5 @@
 import Phaser from 'phaser';
+import Plane from "./objects/Plane"
 
 export default class Demo extends Phaser.Scene {
   plane: Phaser.GameObjects.Sprite;
@@ -14,29 +15,9 @@ export default class Demo extends Phaser.Scene {
   }
 
   create() {
-    this.plane = this.add.sprite(400, 300, "planes", "planeBlue1.png").setScale(0.8, 0.8);
+    this.plane = new Plane(this);
 
-    this.anims.create({
-      key: "planeMoving",
-      frames: this.anims.generateFrameNames(
-        "planes", { prefix: "planeBlue", start: 1, end: 3, suffix: ".png"}
-      ),
-      repeat: -1,
-      yoyo: true,
-      frameRate: 20,
-    });
-
-    this.plane.play("planeMoving");
-
-    this.physics.add.existing(this.plane);
-    this.plane.body.setAllowGravity();
-
-    this.input.on("pointerup", () => this.movePlaneUp(this.plane));
-
-    this.input.keyboard.on("keyup",() => this.movePlaneUp(this.plane));
-  }
-
-  movePlaneUp(plane) {
-    plane.body.setVelocityY(-240)
+    this.input.on("pointerup", () => this.plane.movePlaneUp());
+    this.input.keyboard.on("keyup",() => this.plane.movePlaneUp());
   }
 }
--- src/scenes/objects/Plane.ts
+++ src/scenes/objects/Plane.ts
@@ -3,5 +3,26 @@ import Phaser from "phaser";
 export default class Plane extends Phaser.GameObjects.Sprite {
   constructor(scene) {
     super(scene, 400, 300, "planes", "planeBlue1.png");
+
+    scene.add.existing(this);
+
+    scene.anims.create({
+      key: "planeMoving",
+      frames: scene.anims.generateFrameNames(
+        "planes", { prefix: "planeBlue", start: 1, end: 3, suffix: ".png"}
+      ),
+      repeat: -1,
+      yoyo: true,
+      frameRate: 20,
+    });
+
+    this.play("planeMoving");
+    scene.physics.add.existing(this);
+
+    this.body.setAllowGravity(true);
+  }
+
+  movePlaneUp() {
+    this.body.setVelocityY(-240);
   }
 }
